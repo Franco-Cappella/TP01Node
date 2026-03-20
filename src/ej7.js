68a58121f01dde88349db6837d6e7270b853d897
Android Studio <devandroid@ort>
2026-03-20 12:00:15 -0300

    ej7

---
+++ src/ej7.js
@@ -0,0 +1,18 @@
+//import { getCurrencyAbbreviation, getCurrencyList   } from "currency-map-country"
+import { getCurrencyAbbreviation } from "country-currency-map";
+let monedaDelPais, codigoPais;
+codigoPais = 'Argentina';
+monedaDelPais = obtenerMoneda(codigoPais);
+console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+codigoPais = 'UZA';
+monedaDelPais = obtenerMoneda(codigoPais);
+console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+
+function obtenerMoneda(codigoPais) {
+
+    let msj = getCurrencyAbbreviation(codigoPais)
+    if (msj === undefined) {
+        return null
+    }
+    return msj;
+}
