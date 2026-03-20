10c6a30d3e5742a767fe5e6849554c0fa95e1859
Android Studio <devandroid@ort>
2026-03-20 11:29:11 -0300

    ej7

--- src/ej5.js
+++ src/ej5.js
@@ -3,13 +3,17 @@ let miObjeto = null;
 miUrl = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
 miObjeto = parsearUrl (miUrl);
 console.log(miObjeto);
-function parsearUrl(laURL){
- 
- const url = new URL(laURL);
- return{
- protocolo: url.protocol,
-    host: url.host,
-    puerto: url.port,
- }
-    
+function parsearUrl(laURL) {
+  try {
+    const url = new URL(laURL);
+
+    return {
+      protocolo: url.protocol,
+      host: url.host, 
+      puerto: url.port,
+    };
+  } catch (error) {
+    console.error("Error: La cadena proporcionada no es una URL válida.");
+    return null; 
+  }
 }
